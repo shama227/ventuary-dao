4e24bdcdeb9f5aeadb8b42fb36167aa7813a8994
cvaize <cvaize@gmail.com>
2019-09-15 16:22:54 +0500

    (Feat) Update

---
+++ src/vars.js
@@ -0,0 +1,11 @@
+module.exports = {
+  socials: [
+    { href: 'https://twitter.com/VentuaryDAO', src: '/img/icons/social/tw.svg', alt: 'Twitter' },
+    { href: 'https://www.linkedin.com/company/ventuary/', src: '/img/icons/social/ln.svg', alt: 'LinkedIn' },
+    { href: 'https://t.me/ventuary_dao', src: '/img/icons/social/tg.svg', alt: 'Telegram' },
+    { href: 'https://www.facebook.com/VentuaryDAO', src: '/img/icons/social/fb.svg', alt: 'Facebook' },
+    { href: 'https://medium.com/ventuary-dao', src: '/img/icons/social/md.svg', alt: 'Medium' },
+    { href: 'https://www.youtube.com/channel/UCOTTc5eiq7nAYF47kDD3Rdg', src: '/img/icons/social/yt.svg', alt: 'YouTube' },
+    { href: 'https://www.reddit.com/user/Ventuary-DAO', src: '/img/icons/social/rd.svg', alt: 'Reddit' },
+  ],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,11 @@
 const mix = require('laravel-mix');
-
+const path = require('path');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs')
+const HtmlMinifierPlugin = require('html-minifier-webpack-plugin');
+const vars = require('./src/vars')
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,14 +17,100 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('src/js/app.js', 'dist/js')
+function generateHtmlPlugins(templateDir) {
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        const parts = item.split('.');
+        const name = parts[0];
+        const extension = parts[1];
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+            inject: false,
+            minify: {
+              conservativeCollapse: true,
+              removeRedundantAttributes: true,
+              removeComments: true,
+              collapseInlineTagWhitespace: true,
+              collapseWhitespace: true,
+            }
+        })
+    })
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/template/pages');
+
+mix
+  .webpackConfig({
+      module: {
+          rules: [
+            {
+              test: /\.njk|nunjucks/,
+              use: ['html-loader', { // use html-loader or html-withimg-loader to handle inline resource
+                loader: 'nunjucks-webpack-loader', // add nunjucks-webpack-loader
+                options : {
+                  context: {
+                    ...vars,
+                  },
+                  alias: { // add alias and you can use it in your template
+                    pages : path.resolve(__dirname, './src/template/pages'),
+                    components: path.resolve(__dirname, './src/template/components'),
+                    layouts: path.resolve(__dirname, './src/template/layouts')
+                  },
+                  tags: { // if you want to use different tokens
+                    blockStart: '{%',
+                    blockEnd: '%}',
+                    variableStart: '{{',
+                    variableEnd: '}}',
+                    commentStart: '{#',
+                    commentEnd: '#}'
+                  }
+                }
+              }]
+            },
+            // {
+            //   test: /\.html$/,
+            //   include: path.resolve(__dirname, 'src/html'),
+            //   use: ['raw-loader']
+            // },
+          ]
+      },
+      plugins: [
+          new CopyWebpackPlugin([
+          {
+              from: './src/assets/favicon',
+              to: './favicon'
+          },
+          {
+              from: './src/assets/img',
+              to: './img'
+          },
+          {
+              from: './src/assets/favicon.ico',
+              to: './favicon.ico'
+          },
+          {
+              from: './src/assets/robots.txt',
+              to: './robots.txt'
+          },
+          {
+              from: './src/assets/web.config',
+              to: './web.config'
+          },
+          ]),
+      ].concat(htmlPlugins)
+  })
+  .js('src/js/app.js', 'dist/js')
     .sass('src/sass/app.scss', 'dist/css')
+  .setPublicPath('dist')
 .disableNotifications()
 .browserSync({
     files: [
-        'dist/js/**/*.js',
-        'dist/css/**/*.css',
-        'dist/index.html'
+        'src/**/*.js',
+        'src/**/*.css',
+        'src/**/*.scss',
+        'src/**/*.njk',
+        'src/**/*.html'
     ],
     proxy:
         {
