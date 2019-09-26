467bdd5b027fb4db839e822377b6a13eede1cc7f
Shamil Khalilov <shamfama@gmail.com>
2019-09-26 16:41:56 +0300

    Fix link

--- src/vars.js
+++ src/vars.js
@@ -4,7 +4,7 @@ module.exports = {
     { href: 'https://www.linkedin.com/company/ventuary/', src: '/img/icons/social/ln.svg', alt: 'LinkedIn' },
     { href: 'https://t.me/ventuary_dao', src: '/img/icons/social/tg.svg', alt: 'Telegram' },
     { href: 'https://www.facebook.com/VentuaryDAO', src: '/img/icons/social/fb.svg', alt: 'Facebook' },
-    { href: 'https://medium.com/ventuary-dao', src: '/img/icons/social/md.svg', alt: 'Medium' },
+    { href: 'https://medium.com/@VentuaryDAO', src: '/img/icons/social/md.svg', alt: 'Medium' },
     { href: 'https://www.youtube.com/channel/UCOTTc5eiq7nAYF47kDD3Rdg', src: '/img/icons/social/yt.svg', alt: 'YouTube' },
     { href: 'https://www.reddit.com/user/Ventuary-DAO', src: '/img/icons/social/rd.svg', alt: 'Reddit' },
   ],
